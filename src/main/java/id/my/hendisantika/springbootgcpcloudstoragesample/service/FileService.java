71a4e75400d26ba3dd4af43890c22483a190d4e3
hendisantika <hendisantika@yahoo.co.id>
2025-02-23 07:08:03 +0700

    feat: Add uploadFile in FileService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootgcpcloudstoragesample/service/FileService.java
+++ src/main/java/id/my/hendisantika/springbootgcpcloudstoragesample/service/FileService.java
@@ -1,13 +1,17 @@
 package id.my.hendisantika.springbootgcpcloudstoragesample.service;
 
 import com.google.api.gax.paging.Page;
-import com.google.api.services.storage.Storage;
 import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,5 +54,12 @@ public class FileService {
     public boolean deleteFile(String fileName) {
         Blob blob = storage.get(bucketName, fileName);
         return blob.delete();
+    }
+
+    public void uploadFile(MultipartFile file) throws IOException {
+        BlobId blobId = BlobId.of(bucketName, file.getOriginalFilename());
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).
+                setContentType(file.getContentType()).build();
+        Blob blob = storage.create(blobInfo, file.getBytes());
     }
 }
