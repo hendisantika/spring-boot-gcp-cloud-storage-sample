7171b3c61fddf8b483c56c863895dda96861feeb
hendisantika <hendisantika@yahoo.co.id>
2025-02-23 07:09:44 +0700

    feat: Add uploadFile in FileController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootgcpcloudstoragesample/controller/FileController.java
+++ src/main/java/id/my/hendisantika/springbootgcpcloudstoragesample/controller/FileController.java
@@ -4,9 +4,13 @@ import id.my.hendisantika.springbootgcpcloudstoragesample.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -33,5 +37,13 @@ public class FileController {
         List<String> files = fileService.listOfFiles();
 
         return ResponseEntity.ok(files);
+    }
+
+    //Upload file
+    @PostMapping("upload")
+    public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file) throws IOException {
+        fileService.uploadFile(file);
+
+        return ResponseEntity.ok("File uploaded successfully");
     }
 }
