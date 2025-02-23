ebd98c95d450a8fb4bd692d7e2a440b8b41758d0
hendisantika <hendisantika@yahoo.co.id>
2025-02-23 07:02:29 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootgcpcloudstoragesample/SpringBootGcpCloudStorageSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootgcpcloudstoragesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpCloudStorageSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGcpCloudStorageSampleApplication.class, args);
+    }
+
+}
